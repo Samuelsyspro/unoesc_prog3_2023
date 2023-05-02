a2942bd85e961071d745f003a7b0b442a02448b2
Samuelsyspro <samuel.silva@unoes.edu.br>
2023-05-01 23:09:41 -0300

    Atividades aula 9

---
+++ aula9/routers/groceries.js
@@ -0,0 +1,42 @@
+const {Router} = require('express');
+const router = Router();
+
+const groceryList = [
+    {
+        item: 'milk',
+        quantity: 2,
+    },
+    {
+        item: 'cereal',
+        quantity: 1,
+    },
+    {
+        item: 'pop-tarts',
+        quantity: 1,
+    },
+];
+
+router.get('/', (request, response) => {
+    response.send(groceryList);
+});
+
+router.get('/quantity/:quantity([0-9]+)', (request, response) => {
+    const  quantity = parseInt(request.params.quantity);
+    const groceryByQuantity = groceryList.filter((g) => g.quantity === quantity);
+    response.send(groceryByQuantity);
+});
+
+router.get('/item/:item', (request, response) => {
+    const { item } = request.params;
+    const groceryItem = groceryList.find((g) => g.item === item);
+    response.send(groceryItem);
+});
+
+
+router.post('/', (request, response) => {
+    console.log(request.body);
+    groceryList.push(request.body);
+    response.send(201);
+});
+
+module.exports = router;
